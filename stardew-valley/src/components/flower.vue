c04140302638c0fc976bdae3bb58d7fab91974a3
Alpaca10086zyys <2099193046@qq.com>
2024-07-23 19:52:23 +0800

    更新module

---
+++ stardew-valley/src/components/flower.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div v-for="item of 50" :key="item" :class="['snowflake', item <= 150 ? '' : 'twirl']"></div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="scss">
+.snowflake {
+  --size: 0.4vw;
+  width: var(--size);
+  height: var(--size);
+  background: url('../assets/assistant/flower.png') no-repeat;
+  background-size: 100% 100%;
+  position: fixed;
+  top: -5vh;
+  z-index: 1;
+}
+
+@keyframes snowfall {
+  100% {
+    transform: translate3d(var(--end), 100vh, 0)rotate(3600deg);
+  }
+}
+
+@for $i from 0 through 50 {
+  .snowflake:nth-child(#{$i}) {
+    // 每个花的大小
+    --size: #{random(10) * 0.1}vw;
+    // 花移动目标点 -50后是负数 这样花会向左下方飘落
+    --end: #{random(20) - 50}vw;
+    left: #{random(150)}vw;
+    animation: snowfall #{10 + random(20)}s linear infinite;
+    animation-delay: -#{random(10)}s;
+  }
+}
+
+
+</style>
---
+++ stardew-valley/src/components/snow.vue
@@ -0,0 +1,37 @@
+
+<template>
+  <div v-for="item of 100" :key="item" class="snowflake"></div>
+</template>
+
+<script >
+
+</script>
+
+<style scoped lang="scss">
+.snowflake {
+  --size: 0.4vw;
+  width: var(--size);
+  height: var(--size);
+  background: url('../assets/assistant/snow.png') no-repeat;
+  background-size: 100% 100%;
+  position: fixed;
+  top: -5vh;
+  z-index: 1;
+}
+@keyframes snowfall {
+  100% {
+    transform: translate3d(var(--end), 100vh, 0);
+  }
+}
+
+@for $i from 0 through 100 {
+  .snowflake:nth-child(#{$i}) {
+    //每个雪花的大小
+    --size: #{random(10) * 0.1}vw;
+    //雪花移动目标点 -70后是负数 这样雪花会向左下方飘落
+    --end: #{random(20) - 10}vw;
+    left: #{random(150)}vw;
+    animation: snowfall #{10 + random(20)}s linear infinite;
+    animation-delay: -#{random(10)}s;
+  }
+}</style>
